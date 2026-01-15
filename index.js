96af93a522ed802c2080e94546ccdd5cbfdb313d
Viruna2010 <viruna.randinu1@gmail.com>
2026-01-15 10:14:54 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { spawn } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const port = process.env.PORT || 10000;
+const STREAM_KEY = process.env.STREAM_KEY;
+
+app.get('/', (req, res) => res.send('Viru Radio is Active!'));
+
+function startStreaming() {
+    const musicDir = path.join(__dirname, 'music');
+    let files = fs.readdirSync(musicDir).filter(file => file.endsWith('.mp3'));
+    files.sort(() => Math.random() - 0.5);
+    const playlist = files.map(file => `file '${path.join(musicDir, file)}'`).join('\n');
+    fs.writeFileSync('playlist.txt', playlist);
+
+    const ffmpeg = spawn('ffmpeg', [
+        '-re', '-stream_loop', '-1', '-i', 'video.mp4',
+        '-f', 'concat', '-safe', '0', '-i', 'playlist.txt',
+        '-map', '0:v', '-map', '1:a',
+        '-c:v', 'libx264', '-preset', 'ultrafast', '-b:v', '600k',
+        '-pix_fmt', 'yuv420p', '-g', '60', '-c:a', 'aac', '-b:a', '128k', '-ar', '44100',
+        '-f', 'flv', `rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}`
+    ]);
+
+    ffmpeg.stderr.on('data', (data) => console.log(`FFmpeg: ${data}`));
+    ffmpeg.on('close', () => setTimeout(startStreaming, 5000));
+}
+
+app.listen(port, () => { if (STREAM_KEY) startStreaming(); });
